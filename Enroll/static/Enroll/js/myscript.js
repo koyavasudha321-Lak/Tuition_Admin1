29df5223e2d93c352db761e28d2dfebcad0db6a2
koyavasudha321-Lak <71616728+koyavasudha321-Lak@users.noreply.github.com>
2021-06-19 10:45:38 +0530

    commit done!!

---
+++ Enroll/static/Enroll/js/myscript.js
@@ -0,0 +1,25 @@
+function searchRecords(){
+	var input, table, tr, td, filter, i, txtdata;
+	input = document.getElementById("myInput");
+	filter = input.value.toUpperCase();
+	table = document.getElementById("myTable");
+	tr= table.getElementsByTagName("tr")
+	for(i = 0;i < tr.length;i++){
+		td=tr[i].getElementsByTagName("td")[0];
+		if(td){
+			txtdata = td.textContent || td.innerText;
+			if(txtdata.toUpperCase().indexOf(filter) > -1){
+				tr[i].style.display="";
+			}
+			else{
+				tr[i].style.display="none";
+			}
+		}
+	}
+}
+
+function cleartxt(){
+	document.getElementById('myInput').value="";
+	document.getElementById("myInput").focus();
+}
+
